72bbc50d4b75caf412a9e5f217e6522d23ee4e39
Andre <zorek@me.com>
2024-07-18 19:15:08 +0000

    adcionando cores

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,15 @@
+const textInput = document.querySelector("textarea")
+const buttons = document.querySelectorAll("button")
+const criptografarBtn = document.querySelector(".primary")
+const decriptografarBtn = document.querySelector(".secondary")
+const output = document.querySelector(".output")
 
+textInput.addEventListener("input", () => {
+  if (textInput.value) {
+    buttons.forEach((button) => (button.disabled = false))
+  } else {
+    buttons.forEach((button) => (button.disabled = true))
+  }
+})
+
+criptografarBtn.addEventListener("click", () => console.log("hello"))
