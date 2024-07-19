ab212867ff08daeb3239e6f3f69afe7157b2735e
Andre <zorek@me.com>
2024-07-19 19:00:55 +0000

    melhorias cores com color-mix, criação de elementos. separação de funções

--- scripts/app.js
+++ scripts/app.js
@@ -4,17 +4,44 @@ const criptografarBtn = document.querySelector(".primary")
 const decriptografarBtn = document.querySelector(".secondary")
 const output = document.querySelector(".output")
 
-textInput.addEventListener("input", () => {
-  if (textInput.value) {
+textInput.addEventListener("input", (e) => {
+  enableOrDisableButton(e.target.value)
+})
+
+criptografarBtn.addEventListener("click", () => {
+  clearOutput()
+  output.appendChild(createResultElement(textInput.value))
+  output.appendChild(createCopyBtn())
+})
+
+//funçoes
+
+function clearOutput() {
+  output.innerHTML = ""
+}
+
+function createResultElement(value) {
+  const el = document.createElement("p")
+  el.innerText = value + " alterado"
+
+  return el
+}
+
+function createCopyBtn() {
+  const btn = document.createElement("button")
+  btn.innerText = "Copiar"
+  btn.classList.add("button", "secondary")
+  btn.addEventListener("click", () => {
+    console.log("fui cópiadoooooo")
+  })
+
+  return btn
+}
+
+function enableOrDisableButton(currentInput) {
+  if (currentInput) {
     buttons.forEach((button) => (button.disabled = false))
   } else {
     buttons.forEach((button) => (button.disabled = true))
   }
-})
-
-criptografarBtn.addEventListener("click", () => {
-  const result = document.createElement("p")
-  result.innerText = textInput.value + " alterado"
-  output.innerHTML = ""
-  output.appendChild(result)
-})
+}
