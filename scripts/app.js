b3336ab811fb158f46e776eca8e481c69c559d9a
Andre <zorek@me.com>
2024-07-20 00:28:42 +0000

    funcionalidade principal finalizada

--- scripts/app.js
+++ scripts/app.js
@@ -1,47 +1,139 @@
-const textInput = document.querySelector("textarea")
+const textAreaInput = document.querySelector("textarea")
 const buttons = document.querySelectorAll("button")
 const criptografarBtn = document.querySelector(".primary")
 const decriptografarBtn = document.querySelector(".secondary")
-const output = document.querySelector(".output")
+const outputWrapper = document.querySelector(".output")
 
-textInput.addEventListener("input", (e) => {
-  enableOrDisableButton(e.target.value)
+//funcionalidade principal
+
+textAreaInput.addEventListener("input", (e) => {
+  const value = textAreaInput.value
+  const lastInput = value[value.length - 1]
+  if (charNotAllowed(lastInput)) addWarning()
+
+  textAreaInput.value = value.toLowerCase()
+  toggleButtonState(value)
 })
 
-criptografarBtn.addEventListener("click", () => {
-  clearOutput()
-  output.appendChild(createResultElement(textInput.value))
-  output.appendChild(createCopyBtn())
-})
+buttons.forEach((button) =>
+  button.addEventListener("click", (e) => {
+    clearOutput()
+    const op = e.target.dataset.operation
+    const result =
+      op === "criptografar"
+        ? criptografar(textAreaInput.value)
+        : descriptografar(textAreaInput.value)
 
-//funçoes
+    const outputResult = createResultElement(result)
+    const copyBtn = createCopyBtn(result)
 
-function clearOutput() {
-  output.innerHTML = ""
-}
+    outputWrapper.appendChild(outputResult)
+    outputWrapper.appendChild(copyBtn)
+
+    clearInput()
+    toggleButtonState(textAreaInput.value)
+  })
+)
 
 function createResultElement(value) {
   const el = document.createElement("p")
-  el.innerText = value + " alterado"
+  el.innerText = value
 
   return el
 }
 
-function createCopyBtn() {
+// cria botão para funcionalidade copiar texto (refatorar mais tarde)
+
+function createCopyBtn(value) {
   const btn = document.createElement("button")
   btn.innerText = "Copiar"
   btn.classList.add("button", "secondary")
+
   btn.addEventListener("click", () => {
-    console.log("fui cópiadoooooo")
+    navigator.clipboard.writeText(value)
+    alert("Conteúdo foi copiado para area de transferência")
   })
 
   return btn
 }
 
-function enableOrDisableButton(currentInput) {
+// verifica campo texto e altera estado dos botoes(ativando ou desativando)
+
+function toggleButtonState(currentInput) {
   if (currentInput) {
     buttons.forEach((button) => (button.disabled = false))
   } else {
     buttons.forEach((button) => (button.disabled = true))
   }
+}
+
+// As "chaves" de criptografia que utilizaremos são:
+// A letra "e" é convertida para "enter"
+// A letra "i" é convertida para "imes"
+// A letra "a" é convertida para "ai"
+// A letra "o" é convertida para "ober"
+// A letra "u" é convertida para "ufat"
+function replace(value) {
+  switch (value) {
+    case "e":
+      return "enter"
+    case "i":
+      return "imes"
+    case "a":
+      return "ai"
+    case "o":
+      return "ober"
+    case "u":
+      return "ufat"
+    default:
+      return value
+  }
+}
+
+function criptografar(value) {
+  const stringArr = value.split("")
+  for (let i = 0; i < stringArr.length; i++) {
+    stringArr[i] = replace(stringArr[i])
+  }
+  return stringArr.join("")
+}
+
+const dict = [
+  { src: "e", cipher: "enter" },
+  { src: "i", cipher: "imes" },
+  { src: "a", cipher: "ai" },
+  { src: "o", cipher: "ober" },
+  { src: "u", cipher: "ufat" },
+]
+
+function descriptografar(value) {
+  let result = value
+  dict.forEach(({ src, cipher }) => {
+    result = result.replaceAll(cipher, src)
+  })
+  return result
+}
+
+function clearOutput() {
+  outputWrapper.innerHTML = ""
+}
+
+function clearInput() {
+  textAreaInput.value = ""
+}
+
+// alerta quando digitado caractere "não permitido"
+
+const warning = document.querySelector(".alert")
+
+function addWarning() {
+  warning.classList.add("triggered")
+  setTimeout(() => {
+    warning.classList.remove("triggered")
+  }, 850)
+}
+
+//if char is uppercase return true
+function charNotAllowed(char) {
+  return (char >= "à" && char <= "ÿ") || (char >= "À" && char <= "ß")
 }
